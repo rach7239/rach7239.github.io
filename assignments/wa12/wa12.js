5f7c1768c1e994b89be7e573c5929c98eebe819d
rach7239 <112115128+rach7239@users.noreply.github.com>
2022-11-08 10:41:10 -0700

    push

--- assignments/wa12/wa12.js
+++ assignments/wa12/wa12.js
@@ -1,3 +1,57 @@
+function check() {
+    console.log('test');
+}
+
+function submit() {
+    alert(output.textContent);
+}
+
+function reset() {
+    outputInt = 0;
+    output.textContent = outputInt;
+}
+
+function plusRunner() {
+    plus(outputInt);
+}
+
+function minus() {
+    if (outputInt > 0) {
+    outputInt -=1;
+    output.textContent = outputInt; }
+}
+
+function plus(number) {
+    if (outputInt < 9999999999) {
+    outputInt += 1;
+    output.textContent = outputInt;
+    }
+}
+
+function random() {
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min,max) {
+    const num = Math.floor(Math.random() * (max - min +1)) + min;
+    return num;
+
+}
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plusRunner);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+
+
+/*
+
 const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
@@ -6,4 +60,15 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
+    alert(phone_content.alert);
+}
+
+*/
+
+var slider = document.getElementById("myRange");
+var sliderOutput = document.getQuerySelector(".slider-submit-button").addEventListener('click', update);
+var sliderOutput = document.querySelector(".slider-output");
+
+function update() {
+    sliderOutput.textContent = slider.value;
 }
